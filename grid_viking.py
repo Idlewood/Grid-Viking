36b15f4bf842cfe73e32bc9ff7b9b70cabf78029
Max Lawson <mlawson@gmail.com>
2012-08-12 20:18:32 -0700

    Added hero class, surfaces

--- grid_viking.py
+++ grid_viking.py
@@ -1,8 +1,13 @@
 import pygame, sys
 from pygame.locals import *
 
+screenWidth = 600
+screenHeight = 600
+
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
+
+fpsClock = pygame.time.Clock()
+FPS = 30
 
 WHITE = (255, 255, 255)
 GREEN = (  0, 255,   0)
@@ -17,49 +22,66 @@ worldMap = [ [1, 1, 1, 1, 1, 1, 1],\
              [1, 1, 2, 1, 1, 1, 1] ]
 
 gridColor = GREY
-gridSize = 30
+gridSize = 40
 worldWidth = len(worldMap[0])
 worldHeight = len(worldMap)
 
+mainScreen = pygame.Surface((worldWidth * gridSize, worldHeight * gridSize))
 
-DISPLAYSURF = pygame.display.set_mode((worldWidth * gridSize + 1, worldHeight * gridSize + 1))
-# One pixel is added to the top and side to accomodate the grid line
+
+dungeonFloor = pygame.image.load('sprites/dungeon_floor.png')
+dungeonFloor = pygame.transform.scale(dungeonFloor, (gridSize, gridSize))
+
+dungeonWall = pygame.image.load('sprites/dungeon_wall.png')
+dungeonWall = pygame.transform.scale(dungeonWall, (gridSize, gridSize))
+
+
+DISPLAYSURF = pygame.display.set_mode((screenWidth, screenHeight))
 
 playPosition = [0, 0]
 
+class Hero:
+    def __init__(self, pos):
+        self.x = pos[0]
+        self.y = pos[1]
+        self.sprite = pygame.image.load('sprites/viking.png')
+        self.sprite = pygame.transform.scale(self.sprite, (gridSize, gridSize))
+        
+    def drawHero(self, surface):
+        surface.blit(self.sprite, (self.x * gridSize, self.y * gridSize))
 
+    def moveHero(self, direction):
 
-def isValidMove(x, y, worldMap):
-    mapBoundaryX = len(worldMap)
-    mapBoundaryY = len(worldMap[0])
-    if x in range(0,mapBoundaryX) and y in range(0,mapBoundaryY):
-        if worldMap[x][y] == 1:
-            return True
+        global worldMap
+        if direction == 'up':
+            if self.isValidMove(self.y - 1, self.x, worldMap):
+                self.y = self.y - 1
+        if direction == 'down':
+            if self.isValidMove(self.y + 1, self.x, worldMap):
+                self.y = self.y + 1
+        if direction == 'left':
+            if self.isValidMove(self.y, self.x - 1, worldMap):
+                self.x = self.x - 1
+        if direction == 'right':
+            if self.isValidMove(self.y, self.x + 1, worldMap):
+                self.x = self.x + 1
+
+    def isValidMove(self, x, y, worldMap):
+        mapBoundaryX = len(worldMap)
+        mapBoundaryY = len(worldMap[0])
+        if x in range(0,mapBoundaryX) and y in range(0,mapBoundaryY):
+            if worldMap[x][y] == 1:
+                return True
+            else:
+                return False
         else:
             return False
-    else:
-        return False
-
-def movePlayer(direction):
-    global worldMap
-    global playPosition
-    if direction == 'up':
-        if isValidMove(playPosition[0] - 1, playPosition[1], worldMap):
-            playPosition[0] = playPosition[0] - 1
-    if direction == 'down':
-        if isValidMove(playPosition[0] + 1, playPosition[1], worldMap):
-            playPosition[0] = playPosition[0] + 1
-    if direction == 'left':
-        if isValidMove(playPosition[0], playPosition[1] - 1, worldMap):
-            playPosition[1] = playPosition[1] - 1
-    if direction == 'right':
-        if isValidMove(playPosition[0], playPosition[1] + 1, worldMap):
-            playPosition[1] = playPosition[1] + 1
 
 
-def makeGrid(x1, y1):
-    pygame.draw.line(DISPLAYSURF, gridColor, (x1, y1), (x1, y1 + gridSize), 1)
-    pygame.draw.line(DISPLAYSURF, gridColor, (x1, y1 + gridSize), (x1 + gridSize, y1 + gridSize), 1)
+
+ourHero = Hero((0,0))
+
+
 def makeBorder(worldMap):
     worldWidth = len(worldMap[0]) * gridSize
     worldHeight = len(worldMap) * gridSize
@@ -67,42 +89,44 @@ def makeBorder(worldMap):
     pygame.draw.line(DISPLAYSURF, gridColor, (worldWidth,0), (worldWidth, worldHeight), 1)
 
 def makeWall(x1, y1):
-    pygame.draw.rect(DISPLAYSURF, RED, (x1, y1, gridSize, gridSize))
+    mainScreen.blit(dungeonWall, (x * gridSize, y * gridSize))
 def drawPlayer(playPosition):
     x = playPosition[1]
     y = playPosition[0]
-    pygame.draw.rect(DISPLAYSURF, GREEN, (x * gridSize, y * gridSize, gridSize, gridSize))
+    mainScreen.blit(viking, (x * gridSize, y * gridSize)) 
+
+
 
 while True:
     
-
-
-
     DISPLAYSURF.fill(BLACK)
-    print "x: %s y: %s" % (playPosition[0], playPosition[1])
+    # print "x: %s y: %s" % (playPosition[0], playPosition[1])
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == KEYDOWN:
                 if event.key == K_DOWN:
-                    movePlayer('down')
+                    ourHero.moveHero('down')
                 if event.key == K_UP:
-                    movePlayer('up')
+                    ourHero.moveHero('up')
                 if event.key == K_RIGHT:
-                    movePlayer('right')
+                    ourHero.moveHero('right')
                 if event.key == K_LEFT:
-                    movePlayer('left')
+                    ourHero.moveHero('left')
 
     
+    
+    
+    for y in range(0, len(worldMap)):
+        for x in range(0, len(worldMap[y])):
+                mainScreen.blit(dungeonFloor, (x * gridSize, y * gridSize))
     for y in range(0, len(worldMap)):
         for x in range(0, len(worldMap[y])):
                 if worldMap[y][x] == 2:
                     makeWall(x * gridSize, y * gridSize)
-    drawPlayer(playPosition)
-    for y in range(0, len(worldMap)):
-        for x in range(0, len(worldMap[y])):
-                makeGrid(x * gridSize, y * gridSize)
-           
-    makeBorder(worldMap)
+    #drawPlayer(playPosition)
+    ourHero.drawHero(mainScreen)
+    DISPLAYSURF.blit(mainScreen, (30, 30))      
     pygame.display.update()
+    fpsClock.tick(FPS)
